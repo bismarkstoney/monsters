ff38491713c7176e0aa71de4ccae4eb5deddcdd1
bismarkstoney <bismarkstoney@yahoo.com>
2020-05-07 16:13:54 +0800

    Updates

---
+++ precache-manifest.4ac14b010968daac8809be8b6fccd5e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e883e11ec4f011aed85709178752895",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "3a242f87981b509ff212",
+    "url": "/monsters/static/css/main.a38a7b59.chunk.css"
+  },
+  {
+    "revision": "e460e4046854e53bc271",
+    "url": "/monsters/static/js/2.de6ea3ec.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/monsters/static/js/2.de6ea3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a242f87981b509ff212",
+    "url": "/monsters/static/js/main.a787113d.chunk.js"
+  },
+  {
+    "revision": "48a948fffa185d281f13",
+    "url": "/monsters/static/js/runtime-main.23400953.js"
+  }
+]);
